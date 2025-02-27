d2111b84f3b80ce3c54c91a8f0fa4114226c06cd
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-02-27 12:46:06 +0530

    Commit all the projectof Num counter app

---
+++ script.js
@@ -0,0 +1,22 @@
+const box = document.getElementById("box");
+const countBox = document.getElementById("count");
+let count = 0;
+const err = document.getElementById("error");
+
+function increment() {
+ countBox.textContent = count++;
+ err.textContent = "";
+}
+function decrement() {
+  if (count < 1) {
+    err.textContent = Error("Cannot go below 0");
+  } else {
+   countBox.textContent = count--;
+   err.textContent = "";
+  }
+}
+
+function reset() {
+ countBox.textContent = 0;
+ err.textContent = "";
+}
